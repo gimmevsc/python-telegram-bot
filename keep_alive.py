932e6131047f9b0f2951d5c81ec3eaa4aab9a87b
Bohdan <bodya190505@gmail.com>
2024-04-13 23:37:23 +0200

    2

--- keep_alive.py
+++ keep_alive.py
@@ -1,7 +1,6 @@
-from flask import Flask, send_file
+from flask import Flask, jsonify, send_file
 from threading import Thread
 import os
-import time
 
 app = Flask(__name__)
 
@@ -9,30 +8,20 @@ app = Flask(__name__)
 def index():
     return "Alive"
 
-# Function to download list.db file from the server database folder to the desktop
-def download_list_db():
+@app.route('/get_db_file')
+def get_db_file():
     db_path = os.path.join(os.getcwd(), 'database', 'list.db')
     if os.path.exists(db_path):
-        desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-        destination_path = os.path.join(desktop_path, 'list.db')
-        os.replace(db_path, destination_path)
-        print("list.db downloaded successfully to Desktop")
+        return send_file(db_path, as_attachment=True)
     else:
-        print("Database file not found")
-
-# Main function to download list.db every 1 minute
-def main():
-    while True:
-        download_list_db()
-        time.sleep(60)  # Wait for 1 minute before downloading again
+        return jsonify({"error": "Database file not found"}), 404
 
 def run():
     app.run(host='0.0.0.0', port=8080)
-
+    
 def keep_alive():
     t = Thread(target=run)
     t.start()
-
+    
 if __name__ == "__main__":
     keep_alive()
-    main()
