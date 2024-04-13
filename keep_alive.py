181c758f26ba61e8365100fc66cd55e3c9d9c788
Bohdan <bodya190505@gmail.com>
2024-04-13 16:16:08 +0200

    1

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify, send_file
 from threading import Thread
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Alive"
+    return "Server is alive!"
+
+@app.route('/get_db_file')
+def get_db_file():
+    db_path = os.path.join(os.getcwd(), 'database', 'list.db')
+    if os.path.exists(db_path):
+        return send_file(db_path, as_attachment=True)
+    else:
+        return jsonify({"error": "Database file not found"}), 404
 
 def run():
     app.run(host='0.0.0.0', port=8080)
